ad2fffcd0644908d6ed7854b14ed66f3a0400860
shuya <sz.shuya1231@gmail.com>
2025-03-31 20:53:06 +0900

    done

---
+++ app/components/todo.tsx
@@ -0,0 +1,126 @@
+import React, { useState } from "react";
+import { TodoType } from "../types";
+import { useTodos } from "../hooks/useTodos";
+import { API_URL } from "../constants/url";
+
+type TodoProps = {
+  todo: TodoType;
+};
+
+function Todo({ todo }: TodoProps) {
+  const [isEditing, setIsEditing] = useState<boolean>(false);
+  const [editedTitle, setEditedTitle] = useState<string>(todo.title);
+  const { todos, isLoading, error, mutate } = useTodos();
+
+  const handleEdit = async () => {
+    setIsEditing(!isEditing);
+
+    if (isEditing) {
+      const response = await fetch(
+        `http://localhost:8080/editTodo/${todo.id}`,
+        {
+          method: "PUT",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            title: editedTitle,
+          }),
+        }
+      );
+      if (response.ok) {
+        const editedTodo = await response.json();
+        const udpateTodos = (todos ?? []).map((todo: TodoType) =>
+          todo.id === editedTodo.id ? editedTodo : todo
+        );
+        mutate(udpateTodos, false);
+        mutate();
+      }
+    }
+  };
+
+  const toggleTodoCompletion = async (id: number, isCompleted: boolean) => {
+    const response = await fetch(`${API_URL}/editTodo/${todo.id}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        isCompleted: !isCompleted,
+      }),
+    });
+    if (response.ok) {
+      const editedTodo = await response.json();
+      const udpateTodos = (todos ?? []).map((todo: TodoType) =>
+        todo.id === editedTodo.id ? editedTodo : todo
+      );
+      mutate(udpateTodos, false);
+      mutate();
+    }
+  };
+
+  const handleDelete = async (id: number) => {
+    const response = await fetch(`${API_URL}/delTodo/${todo.id}`, {
+      method: "DELETE",
+      headers: { "Content-Type": "application/json" },
+    });
+    if (response.ok) {
+      const updateTodos = (todos ?? []).filter(
+        (todo: TodoType) => todo.id == id
+      );
+      mutate(updateTodos, false);
+      mutate();
+    }
+  };
+
+  return (
+    <>
+      <li className="py-4">
+        <div className="flex items-center justify-between">
+          <div className="flex items-center">
+            <input
+              id="todo1"
+              name="todo1"
+              type="checkbox"
+              className="h-4 w-4 text-teal-600 focus:ring-teal-500
+            border-gray-300 rounded"
+              onChange={() => toggleTodoCompletion(todo.id, todo.isCompleted)}
+              checked={todo.isCompleted}
+            />
+            <label className="ml-3 block text-gray-900">
+              {isEditing ? (
+                <input
+                  type="text"
+                  value={editedTitle}
+                  onChange={(e) => setEditedTitle(e.target.value)}
+                  className="border rounded py-1 px-2"
+                />
+              ) : (
+                <span
+                  className={`text-lg font-medium mr-2 ${
+                    todo.isCompleted ? "line-through" : ""
+                  }`}
+                >
+                  {" "}
+                  {todo.title}{" "}
+                </span>
+              )}
+            </label>
+          </div>
+          <div className="flex items-center space-x-2">
+            <button
+              onClick={handleEdit}
+              className="duration-150 bg-green-600 hover:bg-green-700 text-white font-medium py-1 px-2 rounded"
+            >
+              {isEditing ? "Save" : "✒"}
+            </button>
+            <button
+              onClick={() => handleDelete(todo.id)}
+              className="bg-red-500 hover:bg-red-600 text-white font-medium py-1 px-2 rounded"
+            >
+              ✖
+            </button>
+          </div>
+        </div>
+      </li>
+    </>
+  );
+}
+
+export default Todo;
---
+++ app/page.tsx
@@ -0,0 +1,84 @@
+"use client";
+import Todo from "./components/todo";
+import { TodoType } from "./types";
+import { useEffect, useRef, useState } from "react";
+import { useTodos } from "./hooks/useTodos";
+import { API_URL } from "./constants/url";
+
+export default function Home() {
+  // タイマー（遊び）
+  const [seconds, setSeconds] = useState(0);
+  const timerId = useRef<ReturnType<typeof setInterval> | null>(null);
+
+  useEffect(() => {
+    timerId.current = setInterval(() => {
+      setSeconds((prev) => prev + 1);
+    }, 1000);
+    return () => {
+      if (timerId.current != null) {
+        clearInterval(timerId.current);
+      }
+    };
+  }, []);
+
+  const { todos, isLoading, error, mutate } = useTodos();
+  // postリクエスト
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const response = await fetch(`${API_URL}/createTodo`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        title: inputRef.current?.value,
+        isCompleted: false,
+      }),
+    });
+
+    if (response.ok) {
+      const newTodo = await response.json();
+      mutate([...(todos ?? []), newTodo], false);
+      mutate();
+      inputRef.current!.value = "";
+    }
+  };
+
+  return (
+    <div className="max-w-md mx-auto bg-white shadow-lg rounded-lg overflow-hidden mt-32 py-4 px-4">
+      <div className="px-4 py-2">
+        <h1 className="text-gray-800 font-bold text-2xl uppercase">
+          To-Do List
+        </h1>
+        <h3>時間経過：{seconds}s</h3>
+      </div>
+      <form
+        className="w-full max-w-sm mx-auto px-4 py-2"
+        onSubmit={handleSubmit}
+      >
+        <div className="flex items-center border-b-2 border-teal-500 py-2">
+          <input
+            className="appearance-none bg-transparent
+      border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight
+      focus:outline-none"
+            type="text"
+            placeholder="Add a task"
+            ref={inputRef}
+          />
+          <button
+            className="duration-150 flex-shrink-0 bg-blue-500 hover:bg-blue-700 border-blue-500 hover:border-blue-700 text-sm border-4 text-white py-1 px-2 rounded"
+            type="submit"
+          >
+            Add
+          </button>
+        </div>
+      </form>
+      <ul className="divide-y divide-gray-200 px-4">
+        {todos?.map((todo: TodoType) => (
+          <Todo key={todo.id} todo={todo} />
+        ))}
+      </ul>
+    </div>
+  );
+}
